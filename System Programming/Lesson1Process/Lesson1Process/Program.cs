22924477fd8b22468d6f9cd20d9dcab35885d0ec
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-09-15 15:04:06 +0400

    Lesson 1. Multitasking, Multithreading, Process, AppDomain

---
+++ System Programming/Lesson1Process/Lesson1Process/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+
+//void foo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("Salam");
+//    }
+//}
+
+//void boo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("Sagol");
+//    }
+//}
+
+//Thread thread = new(foo);
+//thread.Start();
+////foo();
+//boo();
+
+
+//Process.Start("mspaint");
+//Process.Start("notepad");
+//Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+
+//var processes = Process.GetProcesses();
+
+//foreach (var process in processes)
+//{
+//    Console.WriteLine($"{process.ProcessName}");
+//}
+
+
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+
+//var paints = Process.GetProcessesByName("mspaint");
+
+//foreach (var p in paints)
+//{
+//    p.Kill();
+//}
+
+var pr = Process.Start("mspaint");
+
+Console.ReadKey();
+
+pr.Kill();
