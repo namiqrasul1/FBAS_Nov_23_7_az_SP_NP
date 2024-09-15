9ef66e02259ca1775e034cc07226508316b29c6b
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-09-15 15:39:12 +0400

    Lesson 1. Multitasking, Multithreading, Process, AppDomain v2

---
+++ System Programming/Lesson1AppDomain/Lesson1AppDomain/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+
+namespace Lesson1AppDomain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var appDomain = AppDomain.CreateDomain("BabatAppDomain");
+            ////Virus virus = new Virus();
+
+            //Type type = typeof(Virus);
+            ////Console.WriteLine(type.FullName);
+            ////Console.WriteLine(type.Name);
+            ////Console.WriteLine(type.Assembly.FullName);
+
+            //appDomain.CreateInstance(type.Assembly.FullName, type.FullName);
+
+            //AppDomain.Unload(appDomain);
+            //Console.ReadKey();
+
+            var perm = new PermissionSet(PermissionState.None);
+            perm.AddPermission(new FileIOPermission(FileIOPermissionAccess.NoAccess, "C://"));
+
+            var setup = new AppDomainSetup();
+            setup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            var appDomain = AppDomain.CreateDomain("BabatAppDomain", null, setup, perm);
+
+
+            AppDomain.Unload(appDomain);
+
+        }
+    }
+
+    class Virus
+    {
+        public Virus()
+        {
+            Console.WriteLine("Ogurluq vaxtidir!!!");
+        }
+
+        ~Virus()
+        {
+            Console.WriteLine("Ogurluq bitdi. Gedirem!");
+        }
+    }
+}
