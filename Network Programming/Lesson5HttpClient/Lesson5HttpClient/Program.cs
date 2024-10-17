b3ad37e9c05ae08011da4d12b3b4bcf2a1bf6bf4
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-10-17 16:35:41 +0400

    Lesson 6. HTTP

--- Network Programming/Lesson5HttpClient/Lesson5HttpClient/Program.cs
+++ Network Programming/Lesson5HttpClient/Lesson5HttpClient/Program.cs
@@ -1,6 +1,101 @@
-﻿using System.Text.Json;
+﻿using System.Net;
+using System.Text.Json;
 
-using HttpClient client = new();
+class WebHost
+{
+    private short _port;
+    private HttpListener _listener;
+
+    public WebHost(short port)
+    {
+        _port = port;
+    }
+
+    public void Run()
+    {
+        _listener = new HttpListener();
+        _listener.Prefixes.Add($"http://localhost:{_port}/");
+
+        _listener.Start();
+
+        while (true)
+        {
+            var context = _listener.GetContext();
+
+            _ = Task.Run(() => { RequestHandle(context); });
+        }
+    }
+
+    private void RequestHandle(HttpListenerContext context)
+    {
+        var str = context.Request.RawUrl; // /asdasd/home
+
+        Console.WriteLine(context.Request.HttpMethod);
+        Console.WriteLine(context.Request.QueryString);
+
+
+        var path = string.Empty;
+        if (!string.IsNullOrWhiteSpace(str) && str.EndsWith(".jpg"))
+            path = @$"C:\Users\namiqrasullu\Desktop\FBAS_Nov_23_7_az_SP_NP\Network Programming\Lesson5HttpClient\Lesson5HttpClient\Images\{str.Split('/').Last()}";
+        else
+            path = @$"C:\Users\namiqrasullu\Desktop\FBAS_Nov_23_7_az_SP_NP\Network Programming\Lesson5HttpClient\Lesson5HttpClient\Views\{str?.Split('/').Last()}.html";
+
+
+
+        var response = context.Response;
+        var stream = response.OutputStream;
+
+        try
+        {
+            response.ContentType = Path.GetExtension(path) == ".jpg" ? "image/jpg" : "text/html";
+            var src = File.ReadAllBytes(path);
+            stream.Write(src);
+        }
+        catch
+        {
+            var src = File.ReadAllBytes(@$"C:\Users\namiqrasullu\Desktop\FBAS_Nov_23_7_az_SP_NP\Network Programming\Lesson5HttpClient\Lesson5HttpClient\Views\Error.html");
+            stream.Write(src);
+        }
+        finally
+        {
+            stream.Close();
+        }
+    }
+}
+
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        new WebHost(27001).Run();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using HttpClient client = new();
 
 //var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
 
@@ -32,6 +127,7 @@ using HttpClient client = new();
 //    Console.WriteLine(response.StatusCode);
 //}
 
-var response = await client.DeleteAsync("https://jsonplaceholder.typicode.com/posts/5");
+//var response = await client.DeleteAsync("https://jsonplaceholder.typicode.com/posts/5");
 
-Console.WriteLine(response.StatusCode);
+//Console.WriteLine(response.StatusCode);
+
